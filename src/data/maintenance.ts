daa27258ac19f9522f35d66bdeb457826e009727
Jason Brooks <102679906+dorcfandm@users.noreply.github.com>
2026-09-24 08:48:59 -0400

    maintenance

--- src/data/maintenance.ts
+++ src/data/maintenance.ts
@@ -3,5 +3,5 @@ export const maintenance = {
   state: "scheduled", // "info" | "scheduled" | "active"
   title: "Scheduled Maintenance: October 12–13",
   message:
-    "The HPC cluster and research storage will be unavailable Monday, October 12 through Tuesday, October 13 for scheduled maintenance. Running jobs will be stopped, and files on research storage will not be accessible during this time.",
+    "The HPC cluster will be down for maintenance Mon Oct12th- Tues 13th. Running jobs will be stopped, and files on research storage will not be accessible during this time.",
 };
