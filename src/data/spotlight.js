e2cf7324754383600ca1f6071b74a1878e6cb546
Jason Brooks <102679906+dorcfandm@users.noreply.github.com>
2026-09-24 08:59:00 -0400

    Changing Weaver research link to

--- src/data/spotlight.js
+++ src/data/spotlight.js
@@ -3,5 +3,5 @@ export const spotlight = {
   name: 'Dr. Christina Weaver',
   image: '/images/christina-weaver-research.png',
   summary: 'Dr. Christina Weaver and her collaborators explore the structural and biophysical factors that impact signal processing in neurons.',
-  href: '#'
+  href: 'https://www.researchgate.net/publication/398712206_Axon_collateralization_and_focal_myelin_dystrophy_alter_action_potential_propagation_in_multicompartment_pyramidal_neuron_models'
 };
